8508a0b6f3eaed64b167dbeed7b30346ad05b16d
Shayan Shiravani <shayan5420@yahoo.com>
2023-10-16 22:53:02 +0330

    Cleanup

--- scripts/args.js
+++ scripts/args.js
@@ -1,7 +1,4 @@
 const args_abi = [
-  "address", // ChainlinkVRFCoordinator address
-  "uint64", // ChainlinkVRFSubscriptionId
-  "bytes32", // ChainlinkKeyHash
   "uint256", // Muon appId
   "tuple(uint256 x, uint8 parity) d", // Muon publicKey
   "address", // Muon address
@@ -11,31 +8,31 @@ const args_abi = [
 ]
 
 const args_values = [
-  "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
-  2383,
-  "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
   "25465578003500666162514846212575646525905546121357887312364528382074876315425",
   {
     x: "0x701bdf32d0b912560fd66d60723f49caf1ec3e41a727eeb31124f653a9987e3b",
     parity: 1,
   },
-  "0x47206B40a0cfab6D8663D887A20c9398beB35f64",
+  "0xAC00E96dc32241872cA03818F161855C3025f4fA",
   "0x4d7A51Caa1E79ee080A7a045B61f424Da8965A3c",
-  "0xb57490CDAABEDb450df33EfCdd93079A24ac5Ce5",
+  "0xe9239a6Fe26985eB5B21DdaCDc904cDa7f7551a0",
   "0xB10f8E218A9cD738b0F1E2f7169Aa3c0897F2d83"
 ]
 
 // const args_abi = [
-//   "uint256", // Muon appId
-//   "tuple(uint256 x, uint8 parity) d", // Muon publicKey
+//   "address", // ChainlinkVRFCoordinator address
+//   "uint64", // ChainlinkVRFSubscriptionId
+//   "bytes32", // ChainlinkKeyHash
+//   "address", // Admin
+//   "address" // Operator
 // ]
 
 // const args_values = [
-//   "25465578003500666162514846212575646525905546121357887312364528382074876315425",
-//   {
-//     x: "0x701bdf32d0b912560fd66d60723f49caf1ec3e41a727eeb31124f653a9987e3b",
-//     parity: 1,
-//   }
+//   "0xAE975071Be8F8eE67addBC1A82488F1C24858067",
+//   941,
+//   "0xcc294a196eeeb44da2888d17c0625cc88d70d9760a69d58d853ba6581a9ab0cd",
+//   "0xe9239a6Fe26985eB5B21DdaCDc904cDa7f7551a0",
+//   "0xB10f8E218A9cD738b0F1E2f7169Aa3c0897F2d83"
 // ]
 
 module.exports = {
